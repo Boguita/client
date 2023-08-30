9257c39e4de22cb2387ec16607e592a227515f3f
Boguita <heberdgomez@hotmail.com>
2023-08-30 02:56:03 -0300

    deploy prepare

--- src/common/Axiosconfig.jsx
+++ src/common/Axiosconfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
     const api = axios.create({
-      baseURL: "http://localhost:8800/api", // URL base del servidor
+      baseURL: "https://api-test-six-nu.vercel.app/api", // URL base del servidor
       withCredentials: true, // Habilita el envío de cookies
     });
 
