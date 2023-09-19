1e9b42dd9418407b856dd9bd9c61346c37b2e720
Boguita <heberdgomez@hotmail.com>
2023-09-19 08:17:04 -0300

    pass to production

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ import PluginRewriteAll from 'vite-plugin-rewrite-all'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
+  scripts: {
+    "build": "vite build",
+    "preview": "vite preview"
+  },
+
   proxy: {
     // Configura aquí tu proxy
     "/api": {
