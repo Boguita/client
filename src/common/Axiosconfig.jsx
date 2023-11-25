4c86906164885bae518b6617ddac2c17b2a87e96
Boguita <heberdgomez@hotmail.com>
2023-11-25 19:06:47 -0300

    Update API base URL

--- src/common/Axiosconfig.jsx
+++ src/common/Axiosconfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
     const api = axios.create({
-      baseURL: "http://localhost:8800/api", // URL base del servidor
+      baseURL: "https://backuatrebeneficios.galgoproductora.com/api", // URL base del servidor
       withCredentials: true, // Habilita el envío de cookies
     });
 
